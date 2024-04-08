e4ed06a144e8a2d065f3062714cceca5e184339d
yong <dydydade@gmail.com>
2024-04-08 23:27:37 +0900

    [style]RegisteredUserNotFoundException 예외 메세지 수정

--- src/main/java/login/oauthtest4/domain/user/exception/RegisteredUserNotFoundException.java
+++ src/main/java/login/oauthtest4/domain/user/exception/RegisteredUserNotFoundException.java
@@ -3,7 +3,7 @@ package login.oauthtest4.domain.user.exception;
 public class RegisteredUserNotFoundException extends RuntimeException {
 
     public RegisteredUserNotFoundException() {
-        super("기존에 가입한 계정이 없습니다.");
+        super("가입된 계정을 찾을 수 없습니다.");
     }
 
     public RegisteredUserNotFoundException(String message) {
