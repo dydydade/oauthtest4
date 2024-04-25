05241696bc4eb947fb7cc25e2e15f72c8a2f9924
yong <dydydade@gmail.com>
2024-04-25 23:17:48 +0900

    [refactoring] spring data jpa → queryDsl 스타일로 코드 수정

--- src/main/java/login/oauthtest4/domain/user/repository/UserRefreshTokenRepositoryCustom.java
+++ src/main/java/login/oauthtest4/domain/user/repository/UserRefreshTokenRepositoryCustom.java
@@ -1,13 +1,10 @@
 package login.oauthtest4.domain.user.repository;
 
 import login.oauthtest4.domain.user.model.UserRefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-
-public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long>, UserRefreshTokenRepositoryCustom {
+public interface UserRefreshTokenRepositoryCustom {
 
     Optional<UserRefreshToken> findByRefreshToken(String refreshToken);
 
