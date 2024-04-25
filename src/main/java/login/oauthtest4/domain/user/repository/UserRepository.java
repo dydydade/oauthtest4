258b55623f368071b5579fea22f7042a7185835e
yong <dydydade@gmail.com>
2024-04-25 23:16:42 +0900

    [remove] UserRepository 미사용 메서드 삭제

--- src/main/java/login/oauthtest4/domain/user/repository/UserRepository.java
+++ src/main/java/login/oauthtest4/domain/user/repository/UserRepository.java
@@ -2,19 +2,12 @@ package login.oauthtest4.domain.user.repository;
 
 import login.oauthtest4.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
-    @Query("select u from User u left join fetch u.socialProfiles where u.email = :email")
-    Optional<User> findByEmailWithSocialProfiles(String email);
-
-    @Query("select u from User u left join fetch u.socialProfiles sp where sp.socialEmail = :email")
-    Optional<User> checkUserSocialEmailMatched(String email);
 
     Optional<User> findByNickname(String nickname);
 }
