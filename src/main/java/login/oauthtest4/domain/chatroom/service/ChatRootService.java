32e826df70dddaded9c32d5d0ea5a4b2812bed59
n4oah <n4oahdev@gmail.com>
2024-05-23 00:57:13 +0900

    feat: 트랜잭션 추가

--- src/main/java/login/oauthtest4/domain/chatroom/service/ChatRootService.java
+++ src/main/java/login/oauthtest4/domain/chatroom/service/ChatRootService.java
@@ -5,12 +5,15 @@ import login.oauthtest4.domain.chatroom.model.ChatRoom;
 import login.oauthtest4.domain.chatroom.repository.ChatRootRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ChatRootService {
     private final ChatRootRepository chatRootRepository;
 
+    @Transactional
     public Long createChatRoom(
             Long rootManagerUserId,
             CreateChatRoomDto.CreateChatRoomReq createChatRoomReq
