942bd57fe167e63c863d40943ea4f99732f9949e
n4oah <n4oahdev@gmail.com>
2024-05-23 00:57:33 +0900

    feat: 트랜잭션 추가

--- src/main/java/login/oauthtest4/domain/category/service/CategoryService.java
+++ src/main/java/login/oauthtest4/domain/category/service/CategoryService.java
@@ -4,11 +4,13 @@ import login.oauthtest4.domain.category.dto.FindCategoryDto;
 import login.oauthtest4.domain.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
