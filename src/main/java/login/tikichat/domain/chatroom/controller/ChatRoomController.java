5b4fc4b877f5b4592333b4154346dd86cfa9bf3c
yong <dydydade@gmail.com>
2024-07-28 19:38:35 +0900

    [feature] 메세지수 순, 멤버수 순 인기 채팅방 조회 api 추가

--- src/main/java/login/tikichat/domain/chatroom/controller/ChatRoomController.java
+++ src/main/java/login/tikichat/domain/chatroom/controller/ChatRoomController.java
@@ -15,12 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SecurityRequirement(name = "JWT")
 @RestController
@@ -63,15 +58,28 @@ public class ChatRoomController {
         return new ResponseEntity<>(result, HttpStatus.valueOf(result.getStatus()));
     }
 
-    @GetMapping("/ranked")
-    @Operation(summary = "채팅방 조회(인기순)", description = "[홈 화면용] 인기순으로 채팅방을 조회하는 API 입니다.\n[임시] 현재는 임시로 인기순이 아니라 모든 채팅방이 조회됩니다.")
-    public ResponseEntity<ResultResponse> findRankedChatRooms(
-            @RequestParam @Valid FindChatRoomDto.FindChatRoomByPopularityReq findChatRoomReq,
+    @GetMapping("/ranked/message-count")
+    @Operation(summary = "채팅방 조회(메세지수 순)", description = "[홈 화면용] 메세지수 순으로 채팅방을 조회하는 API 입니다.")
+    public ResponseEntity<ResultResponse> findMessageCountRankedChatRooms(
+            @ModelAttribute @Valid FindChatRoomDto.FindChatRoomByPopularityReq findChatRoomReq,
             @AuthenticationPrincipal UserDetailInfo user
     ) {
         ResultResponse result = ResultResponse.of(
                 ResultCode.FIND_CHAT_ROOMS_SUCCESS,
-                this.chatRoomService.findChatRoomsByPopularity(findChatRoomReq, user.getUserId())
+                this.chatRoomService.findMessageCountRankedChatRooms(findChatRoomReq, user.getUserId())
+        );
+        return new ResponseEntity<>(result, HttpStatus.valueOf(result.getStatus()));
+    }
+
+    @GetMapping("/ranked/member-count")
+    @Operation(summary = "채팅방 조회(멤버수 순)", description = "[홈 화면용] 멤버수 순으로 채팅방을 조회하는 API 입니다.")
+    public ResponseEntity<ResultResponse> findMemberCountRankedChatRooms(
+            @ModelAttribute @Valid FindChatRoomDto.FindChatRoomByPopularityReq findChatRoomReq,
+            @AuthenticationPrincipal UserDetailInfo user
+    ) {
+        ResultResponse result = ResultResponse.of(
+                ResultCode.FIND_CHAT_ROOMS_SUCCESS,
+                this.chatRoomService.findMemberCountRankedChatRooms(findChatRoomReq, user.getUserId())
         );
         return new ResponseEntity<>(result, HttpStatus.valueOf(result.getStatus()));
     }
--- src/main/java/login/tikichat/domain/chatroom/service/ChatRoomService.java
+++ src/main/java/login/tikichat/domain/chatroom/service/ChatRoomService.java
@@ -8,14 +8,20 @@ import login.tikichat.domain.chatroom.dto.FindChatRoomDto;
 import login.tikichat.domain.chatroom.model.ChatRoom;
 import login.tikichat.domain.chatroom.repository.ChatRoomRepository;
 import login.tikichat.domain.chatroom_participant.service.ChatRoomParticipantService;
+import login.tikichat.domain.top_ranked_chatroom.member_count.model.MemberCountRankedChatRoom;
+import login.tikichat.domain.top_ranked_chatroom.member_count.repository.MemberCountRankedChatRoomRepository;
+import login.tikichat.domain.top_ranked_chatroom.message_count.model.MessageCountRankedChatRoom;
+import login.tikichat.domain.top_ranked_chatroom.message_count.repository.MessageCountRankedChatRoomRepository;
 import login.tikichat.global.exception.BusinessException;
 import login.tikichat.global.exception.ErrorCode;
 import login.tikichat.global.exception.chatroom.ChatRoomNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +30,9 @@ public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
     private final CategoryRepository categoryRepository;
     private final ChatRoomParticipantService chatRoomParticipantService;
+    private final MessageCountRankedChatRoomRepository messageCountRankedChatRoomRepository;
+    private final MemberCountRankedChatRoomRepository memberCountRankedChatRoomRepository;
+
 
     @Transactional
     public Long createChatRoom(
@@ -55,56 +64,73 @@ public class ChatRoomService {
     ) {
         final var chatRooms = this.chatRoomRepository.findChatRooms(findChatRoomReq, userId);
 
-        return new FindChatRoomDto.FindChatRoomRes(
-                chatRooms.stream().map((chatRoom ->
-                        new FindChatRoomDto.FindChatRoomItemRes(
-                                chatRoom.getName(),
-                                chatRoom.getMaxUserCount(),
-                                chatRoom.getCurrentUserCount(),
-                                chatRoom.getTags(),
-                                chatRoom.getRoomManagerUserId(),
-                                new FindCategoryDto.FindCategoryItemRes(
-                                        chatRoom.getCategory().getCode(),
-                                        chatRoom.getCategory().getName(),
-                                        chatRoom.getCategory().getOrderNum()
-                                )
-                        )
-                )).toList()
-        );
-    }
-
-    public FindChatRoomDto.FindChatRoomRes findChatRoomsByPopularity(
-            FindChatRoomDto.FindChatRoomByPopularityReq findChatRoomReq,
-            Long userId
-    ) {
-        // TODO: 추후 채팅방 인기순으로 상위 N개 조회하여 반환하도록 수정 필요
-        final var chatRooms = this.chatRoomRepository.findAll();
-
-        return new FindChatRoomDto.FindChatRoomRes(
-                chatRooms.stream().map((chatRoom ->
-                        new FindChatRoomDto.FindChatRoomItemRes(
-                                chatRoom.getName(),
-                                chatRoom.getMaxUserCount(),
-                                chatRoom.getCurrentUserCount(),
-                                chatRoom.getTags(),
-                                chatRoom.getRoomManagerUserId(),
-                                new FindCategoryDto.FindCategoryItemRes(
-                                        chatRoom.getCategory().getCode(),
-                                        chatRoom.getCategory().getName(),
-                                        chatRoom.getCategory().getOrderNum()
-                                )
-                        )
-                )).toList()
-        );
+        return convertToFindChatRoomRes(chatRooms);
     }
 
     public ChatRoom findById(Long chatRoomId) {
-        return chatRoomRepository.findById(chatRoomId).orElseThrow(() -> new ChatRoomNotFoundException());
+        return chatRoomRepository.findById(chatRoomId).orElseThrow(ChatRoomNotFoundException::new);
     }
 
     @Transactional
     public void linkAttachment(Long chatRoomId, Attachment attachment) {
-        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(() -> new ChatRoomNotFoundException());
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(ChatRoomNotFoundException::new);
         chatRoom.addAttachment(attachment);
+    }
+
+
+    public FindChatRoomDto.FindChatRoomRes findMessageCountRankedChatRooms(
+            FindChatRoomDto.FindChatRoomByPopularityReq findChatRoomReq,
+            Long userId
+    ) {
+        final var chatRooms = this.findMessageCountChatRooms(findChatRoomReq).stream()
+                .map(rankedChatRoom -> chatRoomRepository
+                        .findById(rankedChatRoom.getChatRoomId())
+                        .orElseThrow(ChatRoomNotFoundException::new))
+                .toList();
+
+        return convertToFindChatRoomRes(chatRooms);
+    }
+
+    private List<MessageCountRankedChatRoom> findMessageCountChatRooms(FindChatRoomDto.FindChatRoomByPopularityReq request) {
+        Pageable limit = PageRequest.of(0, request.popularityRank());
+        return messageCountRankedChatRoomRepository.findMessageCountChatRooms(request.categoryCode(), limit);
+    }
+
+
+    public FindChatRoomDto.FindChatRoomRes findMemberCountRankedChatRooms(
+            FindChatRoomDto.FindChatRoomByPopularityReq findChatRoomReq,
+            Long userId
+    ) {
+        final var chatRooms = this.findMemberCountChatRooms(findChatRoomReq).stream()
+                .map(rankedChatRoom -> chatRoomRepository
+                        .findById(rankedChatRoom.getChatRoomId())
+                        .orElseThrow(ChatRoomNotFoundException::new))
+                .toList();
+
+        return convertToFindChatRoomRes(chatRooms);
+    }
+
+    private List<MemberCountRankedChatRoom> findMemberCountChatRooms(FindChatRoomDto.FindChatRoomByPopularityReq request) {
+        Pageable limit = PageRequest.of(0, request.popularityRank());
+        return memberCountRankedChatRoomRepository.findMemberCountChatRooms(request.categoryCode(), limit);
+    }
+
+    private FindChatRoomDto.FindChatRoomRes convertToFindChatRoomRes(List<ChatRoom> chatRooms) {
+        return new FindChatRoomDto.FindChatRoomRes(
+                chatRooms.stream().map((chatRoom ->
+                        new FindChatRoomDto.FindChatRoomItemRes(
+                                chatRoom.getName(),
+                                chatRoom.getMaxUserCount(),
+                                chatRoom.getCurrentUserCount(),
+                                chatRoom.getTags(),
+                                chatRoom.getRoomManagerUserId(),
+                                new FindCategoryDto.FindCategoryItemRes(
+                                        chatRoom.getCategory().getCode(),
+                                        chatRoom.getCategory().getName(),
+                                        chatRoom.getCategory().getOrderNum()
+                                )
+                        )
+                )).toList()
+        );
     }
 }
